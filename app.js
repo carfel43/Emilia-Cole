c1b9c986241810e4f140ffe625b3427363256a6c
carfel43 <gimenezcar201518@gmail.com>
2023-10-13 14:48:14 -0300

    se agrego la extructuras de carpetas y html

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express();
+const path = require("path");
+
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname , "/views/index.html"))
+});
+
+app.use(express.static("public"));
+
+app.get("/music", (req,res)=>{
+    res.sendFile(path.join(__dirname , "/views/music.html"))
+});
+
+app.get("/contact",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/contact.html"));
+});
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+app.listen(3000, ()=>{
+    console.log("servidor corriendo en el puerto 3000");
+} )
